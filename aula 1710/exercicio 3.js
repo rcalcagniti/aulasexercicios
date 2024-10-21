fa25a67b88b2974d0546d6f65701d5cd0d1f455b
rcalcagniti <rcalcagniti@gmail.com>
2024-10-21 08:42:02 -0300

    criacao do file de html e js e ja efetuei o exercicio

---
+++ aula 1710/exercicio 3.js
@@ -0,0 +1,10 @@
+// outro tipo de comentário - Função criada em um ficheiro externo que mostra uma mensagem
+
+/* tentando adicionar um comentário - Função criada em um ficheiro externo que mostra uma
+ mensagem - esté pode ter comentário de várias linhas*/
+
+document.getElementById("botao").addEventListener("click", function() {
+  alert("AIA");
+});
+
+/* peguei a ideia do codigo em: https://www.shecodes.io/athena/40000-how-to-create-a-button-with-click-alert-in-javascript */
